5128b64343349f76d44a63d847cfb211e81c5a0c
Jeom1028 <apam144@gmail.com>
2024-06-12 17:14:00 +0900

    Initial Commit

---
+++ BaseballGame/BaseballGame.swift
@@ -0,0 +1,66 @@
+// BaseballGame.swift 파일 생성
+struct BaseballGame {
+    func start() {
+        let answer = makeAnswer() // 정답을 만드는 함수
+        
+        print("게임을 시작하겠습니다")
+        
+        while true {
+            print("숫자를 입력하세요", terminator: " ")
+            
+            guard let input = readLine() else {
+                continue
+            }
+            
+            guard let number = Int(input) else {
+                print("잘못된 입력입니다")
+                continue
+            }
+            
+            guard input.count == 3 else {
+                print("세 자리 숫자를 입력하세요")
+                continue
+            }
+            
+            let digits = String(number).compactMap { Int(String($0)) }
+            
+            guard digits.count == 3, Set(digits).count == 3 else {
+                
+                print("잘못된 입력입니다")
+                continue
+            }
+            
+            var strike = 0
+            var ball = 0
+            
+            for (index, digit) in digits.enumerated() {
+                if digit == answer[index] {
+                    strike += 1
+                } else if answer.contains(digit) {
+                    ball += 1
+                }
+            }
+            
+            if strike == 3 {
+                print("정답입니다!")
+                break
+            } else {
+                print("\(strike)스트라이크 \(ball)볼")
+            }
+        }
+    }
+    
+    func makeAnswer() -> [Int] {
+        // 함수 내부를 구현하기
+        var digits = [Int]()
+        
+        while digits.count < 3 {
+            let digit = Int.random(in: 1...9)
+            if !digits.contains(digit) {
+                digits.append(digit)
+            }
+        }
+        
+        return digits
+    }
+}
---
+++ BaseballGame/main.swift
@@ -0,0 +1,7 @@
+// main.swift 파일
+// 프로젝트 생성시 자동 생성됨
+
+let game = BaseballGame()
+game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+
+

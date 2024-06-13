40d94b532b49f092ca0acf7f39c2f8f4da84e98e
Jeom1028 <apam144@gmail.com>
2024-06-13 17:10:32 +0900

    Lv 5 수정

--- BaseballGame/BaseballGame.swift
+++ BaseballGame/BaseballGame.swift
@@ -1,11 +1,12 @@
-// BaseballGame.swift 파일 생성
-
 class BaseballGame {
-    let recordManger = RecordManager()
+
+    var tryCnt: [Int] = [] // 시도 횟수를 저장하는 배열
+    var count: Int = 0 // 게임 횟수를 저장하는 변수
     
     func start() {
+        print("환영합니다! 원하시는 번호를 입력해주세요")
         while true {
-            print("1.게임 시작하기 2.게임 기록보기 3.종료하기")
+            print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
             print("선택하세요", terminator: " ")
             
             guard let choice = readLine(), let option = Int(choice) else {
@@ -17,82 +18,85 @@ class BaseballGame {
             case 1:
                 play()
             case 2:
-                recordManger.showRecords()
+                print("< 게임 기록 보기 >")
+                for i in 0..<count {
+                    print("\(i + 1)번째 게임 : 시도 횟수 - \(tryCnt[i])")
+                }
             case 3:
-                print ("게임을 종료하겠습니다")
+                print("게임을 종료하겠습니다")
                 return
             default:
                 print("잘못입력하셨습니다")
             }
         }
+    }
+
+    func play() {
+        let answer = makeAnswer() // 정답을 만드는 함수
+        var trialCount = 0
+
+        print("게임을 시작하겠습니다!")
         
+        while true {
+            print("숫자를 입력하세요", terminator: " ")
+
+            guard let input = readLine() else {
+                continue
+            }
+
+            guard let number = Int(input) else {
+                print("잘못된 입력입니다")
+                continue
+            }
+
+            guard input.count == 3 else {
+                print("세 자리 숫자를 입력하세요")
+                continue
+            }
+
+            let digits = String(number).compactMap { Int(String($0)) }
+
+            guard digits.count == 3, Set(digits).count == 3 else {
+                print("잘못된 입력입니다")
+                continue
+            }
+
+            trialCount += 1
+            var strike = 0
+            var ball = 0
+
+            for (index, digit) in digits.enumerated() {
+                if digit == answer[index] {
+                    strike += 1
+                } else if answer.contains(digit) {
+                    ball += 1
+                }
+            }
+
+            if strike == 3 {
+                print("정답입니다!")
+                break
+            } else {
+                print("\(strike)스트라이크 \(ball)볼")
+            }
+        }
+
+        // 게임이 끝난 후 시도 횟수를 기록하고 게임 횟수를 증가
+        tryCnt.append(trialCount)
+        count += 1
+    }
+
+    func makeAnswer() -> [Int] {
+        var digits = [Int]()
         
-         func play() {
-            let answer = makeAnswer() // 정답을 만드는 함수
-            var trialCount = 0
-             
-            print("게임을 시작하겠습니다!")
-            
-            while true {
-                print("숫자를 입력하세요", terminator: " ")
-                
-                guard let input = readLine() else {
-                    continue
-                }
-                
-                guard let number = Int(input) else {
-                    print("잘못된 입력입니다")
-                    continue
-                }
-                
-                guard input.count == 3 else {
-                    print("세 자리 숫자를 입력하세요")
-                    continue
-                }
-                
-                
-                let digits = String(number).compactMap { Int(String($0)) }
-                
-                guard digits.count == 3, Set(digits).count == 3 else {
-                    
-                    print("잘못된 입력입니다")
-                    continue
-                }
-                
-                trialCount += 1
-                var strike = 0
-                var ball = 0
-                
-                for (index, digit) in digits.enumerated() {
-                    if digit == answer[index] {
-                        strike += 1
-                    } else if answer.contains(digit) {
-                        ball += 1
-                    }
-                }
-                
-                if strike == 3 {
-                    print("정답입니다!")
-                    break
-                } else {
-                    print("\(strike)스트라이크 \(ball)볼")
-                }
+        while digits.count < 3 {
+            let digit = Int.random(in: digits.isEmpty ? 1...9 : 0...9)
+            if !digits.contains(digit) {
+                digits.append(digit)
             }
         }
         
-        func makeAnswer() -> [Int] {
-            // 함수 내부를 구현하기
-            var digits = [Int]()
-            
-            while digits.count < 3 {
-                //첫번째 숫자가 0이 안나오게 하기
-                let digit = Int.random(in: digits.isEmpty ? 1...9 : 0...9)
-                if !digits.contains(digit) {
-                    digits.append(digit)
-                }
-            }
-            
-            return digits
-        }
+        return digits
     }
 }
+
--- BaseballGame/RecordManger.swift
+++ BaseballGame/RecordManger.swift
@@ -1,19 +1,1 @@
 
-class RecordManager {
-    private var records = [Int]()
-    
-    func add(traialCount: Int){
-        records.append(traialCount)
-    }
-    
-    func showRecords() {
-        if records.isEmpty {
-            print("기록이 없습니다")
-        } else {
-            print("게임 기록")
-            for (index,record) in records.enumerated() {
-                print("\(index + 1)번째 게임 : 시도 횟수 - \(record)")
-            }
-        }
-    }
-}
